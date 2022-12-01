7e06d934e8ef74707451ba378248234895e84ea3
Abdul Khaliq <Akhunzaada@gmail.com>
2022-12-02 02:12:32 +0500

    Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.split { it.isEmpty() }
+            .maxOf { it.sumOf { calories -> calories.toInt() } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.split { it.isEmpty() }
+            .map { it.sumOf { calories -> calories.toInt() } }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,16 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Splits the original list into sub-lists using the given [predicate].
+ */
+fun List<String>.split(predicate: (String)-> Boolean): List<List<String>> {
+    return flatMapIndexed { i, v ->
+        when {
+            i == 0 || i == lastIndex -> listOf(i)
+            predicate(v) -> listOf(i - 1, i + 1)
+            else -> emptyList()
+        }
+    }.windowed(size = 2, step = 2) { (from, to) -> slice(from..to) }
+}
